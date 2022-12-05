bb7b3b2afcf7c9d456615e4faf2b67138d9ef7d4
juanelertora <juanelertora@gmail.com>
2022-12-05 16:00:33 -0300

    header&footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom"
+
+import Productos from "./pages/Productos";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      Hola
+          <div>
+        <Header/>
+      </div>
+      <div>
+      <Routes>
+        <Route path="productos" element={ <Productos/>}/>
+        <Route path="home" element={<Home/>}/>
+      </Routes>
+      </div>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
